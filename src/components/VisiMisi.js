7fcc4003f3647e5ab3d53a68a7b42731841f7001
zelsaddr <zeldin26@gmail.com>
2023-02-12 19:48:34 +0700

    add change

--- src/components/VisiMisi.js
+++ src/components/VisiMisi.js
@@ -1,31 +1,34 @@
 const VisionAndMission = () => {
   return (
-    <div className="py-2">
-      <div className="mb-6">
-        <h2 className="text-2xl font-bold mb-2 italic">Visi</h2>
-        <ul>
-          <li className="list-disc ml-10">
-            <p className="text-white text-base">Mempererat Silaturahmi</p>
-          </li>
-          <li className="list-disc ml-10">
-            <p className="text-white text-base">Mempererat Kekompakan</p>
-          </li>
-          <li className="list-disc ml-10">
-            <p className="text-white text-base">Menjalin Kebersamaan</p>
-          </li>
-          <li className="list-disc ml-10">
-            <p className="text-white text-base">Menjalin Organisasi Yang Sama</p>
-          </li>
-        </ul>
+    <>
+      <img className="object-cover w-screen h-52 rounded-lg" src="/prediksisantai.jpg" alt="Article" />
+      <div className="py-2">
+        <div className="mb-6">
+          <h2 className="text-2xl font-bold mb-2 italic">Visi</h2>
+          <ul>
+            <li className="list-disc ml-10">
+              <p className="text-white text-base">Mempererat Silaturahmi</p>
+            </li>
+            <li className="list-disc ml-10">
+              <p className="text-white text-base">Mempererat Kekompakan</p>
+            </li>
+            <li className="list-disc ml-10">
+              <p className="text-white text-base">Menjalin Kebersamaan</p>
+            </li>
+            <li className="list-disc ml-10">
+              <p className="text-white text-base">Menjalin Organisasi Yang Sama</p>
+            </li>
+          </ul>
+        </div>
+        <div className="mb-6">
+          <h2 className="text-2xl font-bold mb-2 italic">Misi</h2>
+          <p className="text-white text-base ml-6 text-justify w-5/12">
+            Dengan adanya komunitas The Prediksi ini diharapkan bisa untuk ajang silahturahmi untuk para anggota. Dan selain itu semoga komunitas ini bisa membuat kebersamaan yang kuat untuk semua anggota. The Prediksi ini ingin menyatukan
+            semua pikiran-pikiran yang berbeda untuk menuju satu tujuan yang sama.
+          </p>
+        </div>
       </div>
-      <div className="mb-6">
-        <h2 className="text-2xl font-bold mb-2 italic">Misi</h2>
-        <p className="text-white text-base ml-6 text-justify w-5/12">
-          Dengan adanya komunitas The Prediksi ini diharapkan bisa untuk ajang silahturahmi untuk para anggota. Dan selain itu semoga komunitas ini bisa membuat kebersamaan yang kuat untuk semua anggota. The Prediksi ini ingin menyatukan
-          semua pikiran-pikiran yang berbeda untuk menuju satu tujuan yang sama.
-        </p>
-      </div>
-    </div>
+    </>
   );
 };
 
